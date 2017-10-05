e380e7c3b4ab434c92eebf0b294a9de2e30777b8
Thomas Munoz <thomas.munoz30@gmail.com>
2017-10-05 17:18:49 +0200

    [WIP] ApproverService - empty webservice for now

---
+++ services/approver/src/main/java/data/BusinessTravelRequest.java
@@ -0,0 +1,34 @@
+package data;
+
+import hotel.service.Hotel;
+import paf.Flight;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+import java.util.List;
+
+@XmlType
+public class BusinessTravelRequest {
+    // May be missing some fields
+
+    private BusinessTravelRequestStatus status;
+    private List<Flight> flghts;
+    private List<Hotel> hotels;
+    private List<Car> cars;
+
+    @XmlElement(name = "status", required = true)
+    public BusinessTravelRequestStatus getStatus() { return status; }
+    public void setStatus(BusinessTravelRequestStatus status) { this.status = status; }
+
+    @XmlElement
+    public List<Flight> getFlghts() { return flghts; }
+    public void setFlghts(List<Flight> flghts) { this.flghts = flghts; }
+
+    @XmlElement
+    public List<Hotel> getHotels() { return hotels; }
+    public void setHotels(List<Hotel> hotels) { this.hotels = hotels; }
+
+    @XmlElement
+    public List<Car> getCars() { return cars; }
+    public void setCars(List<Car> cars) { this.cars = cars; }
+}
---
+++ services/approver/src/main/java/data/BusinessTravelRequestStatus.java
@@ -0,0 +1,8 @@
+package data;
+
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType
+public enum BusinessTravelRequestStatus {
+    WAITING, APPROVED, DENIED
+}
