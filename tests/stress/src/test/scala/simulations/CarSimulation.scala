5ab05255b949af2b05ea3d08719b1f7dadbcdc79
Loris Friedel <loris.friedel@gmail.com>
2017-10-08 17:08:26 +0200

    Update Car stress test to match real service usage

--- tests/stress/src/test/scala/simulations/CarSimulation.scala
+++ tests/stress/src/test/scala/simulations/CarSimulation.scala
@@ -1,54 +1,67 @@
-package computerdatabase
-
-import java.util.UUID
+package simulations
 
 import scala.language.postfixOps
-
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
-import java.util.Random
+import io.gatling.http.protocol.HttpProtocolBuilder
+
 import scala.concurrent.duration._
+import scala.util.Random
 
 class CarSimulation extends Simulation {
-  val httpConf =
+  val httpConf: HttpProtocolBuilder =
     http
-      .baseURL("http://localhost:8080/car/")
+      .baseURL("http://localhost:8080/car-service-document/")
       .acceptHeader("application/json")
       .header("Content-Type", "application/json")
 
-  val stressSample =
-    scenario("find car")
-      .repeat(100) {
-        exec(session =>
-          session.set("model", "S")// Integer.toString(new Random.nextInt (26) + 97))
-        )
+  val stressSample: ScenarioBuilder =
+    scenario("Find available cars in Nice")
+      .repeat(16) {
+        exec(session => {
+          session
+            .set("dateFrom", "2017-" + (Random.nextInt(5) + 1).toString + "-" + (Random.nextInt(14) + 1).toString)
+            .set("dateTo", "2017-" + (Random.nextInt(5) + 7).toString + "-" + (Random.nextInt(12) + 16).toString)
+            .set("city", Random.nextString(Random.nextInt(10) + 3))
+        })
           .exec(
-            http("find a car ")
+            http("Find cars by date")
               .post("car")
-              .body(StringBody(session => buildRegister(session)))
+              .body(StringBody(session => buildSearchByDate(session)))
+              .check(status.is(200))
+          )
+          .pause(1 seconds)
+          .exec(
+            http("Find cars by city")
+              .post("car")
+              .body(StringBody(session => buildSearchByCity(session)))
               .check(status.is(200))
           )
       }
 
-  def buildRegister(session: Session): String = {
-    val model = session("model").as[String]
+  def buildSearchByDate(session: Session): String = {
+    val dateFrom = session("dateFrom").as[String]
+    val dateTo = session("dateTo").as[String]
+
     raw"""{
-      "company" : "Tesla",
       "city" : "Nice",
-      "model" : "$model",
-      "numberPlate" : "999-111",
-      "bookedDays" : [{ "date" : "2017-12-24" }]
+      "dateFrom" : "$dateFrom",
+      "dateTo" : "$dateTo",
       }
       """"
-
   }
 
-  def buildRetrieve(session: Session): String = {
-    val model = session("model").as[String]
+  def buildSearchByCity(session: Session): String = {
+    val city = session("city").as[String]
+
     raw"""{
-      "model": "$model"
-    }""""
-
+      "city" : "$city",
+      "dateFrom" : "2017-12-1",
+      "dateTo" : "2017-12-18",
+      }
+      """"
   }
-  setUp(stressSample.inject(rampUsers(20) over (10 seconds)).protocols(httpConf))
+
+  setUp(stressSample.inject(rampUsers(2048) over (64 seconds)).protocols(httpConf))
 }
