100b6ea2f297a932b854c3e18af2779cf1fe6e12
cesar colle <cesar.colle@gmail.com>
2017-10-08 15:03:21 +0200

    Add simple stress test for Car Simulation --> a lot of user ask database for a model S car ! initialization : ./build.sh && ./start.sh use : cd ./stress && mvn -q gatling:execute to compute.

--- tests/stress/src/test/scala/simulations/CarSimulation.scala
+++ tests/stress/src/test/scala/simulations/CarSimulation.scala
@@ -6,42 +6,49 @@ import scala.language.postfixOps
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-
+import java.util.Random
 import scala.concurrent.duration._
-import org.bson.Document
 
 class CarSimulation extends Simulation {
   val httpConf =
     http
-      .baseURL("http://localhost:9080/car/")
+      .baseURL("http://localhost:8080/car/")
       .acceptHeader("application/json")
       .header("Content-Type", "application/json")
 
   val stressSample =
     scenario("find car")
-      .repeat(10) {
+      .repeat(100) {
         exec(session =>
-          session.set()
+          session.set("model", "S")// Integer.toString(new Random.nextInt (26) + 97))
         )
           .exec(
             http("find a car ")
-              .post("registry")
+              .post("car")
               .body(StringBody(session => buildRegister(session)))
               .check(status.is(200))
           )
       }
 
   def buildRegister(session: Session): String = {
-    val ssn = session("ssn").as[String]
-    raw"""
-    { "company" : "Tesla",
+    val model = session("model").as[String]
+    raw"""{
+      "company" : "Tesla",
       "city" : "Nice",
-      "model" : "S",
+      "model" : "$model",
       "numberPlate" : "999-111",
       "bookedDays" : [{ "date" : "2017-12-24" }]
       }
-      """
+      """"
 
   }
 
+  def buildRetrieve(session: Session): String = {
+    val model = session("model").as[String]
+    raw"""{
+      "model": "$model"
+    }""""
+
+  }
+  setUp(stressSample.inject(rampUsers(20) over (10 seconds)).protocols(httpConf))
 }
