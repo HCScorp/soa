d04e581835c119840676470ee3a625df83bf1ecb
cesar colle <cesar.colle@gmail.com>
2017-10-08 16:39:20 +0200

    Add proper Car stress scenario for Car Service : - User choose random date first for a default modele. - User will after check other model for the date. - user will retry for other date/ model 10 times.
    next step : create stress sceario for other services

--- tests/stress/src/test/scala/simulations/CarSimulation.scala
+++ tests/stress/src/test/scala/simulations/CarSimulation.scala
@@ -1,12 +1,13 @@
 package computerdatabase
 
+import java.time.LocalDate
 import java.util.UUID
 
 import scala.language.postfixOps
-
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import java.util.Random
+
 import scala.concurrent.duration._
 
 class CarSimulation extends Simulation {
@@ -18,19 +19,54 @@ class CarSimulation extends Simulation {
 
   val stressSample =
     scenario("find car")
-      .repeat(100) {
-        exec(session =>
-          session.set("model", "S")// Integer.toString(new Random.nextInt (26) + 97))
+      .repeat(6) {
+        exec(session => {
+          session.set("date", randomDay())
+          session.set("model", Integer.toString(new Random().nextInt(26) + 97))
+        }
+
         )
           .exec(
-            http("find a car ")
+            http("find a car with different date")
               .post("car")
-              .body(StringBody(session => buildRegister(session)))
+              .body(StringBody(session => buildCarDate(session)))
               .check(status.is(200))
           )
+
+          .pause(1 seconds)
+          .exec(
+            http("find a car with the model")
+              .post("car")
+              .body(StringBody(session => buildCarModel(session)))
+              .check(status.is(200))
+          )
+
       }
 
-  def buildRegister(session: Session): String = {
+
+  def randomDay(): String = {
+    val minDate = LocalDate.of(2017, 1, 10).toEpochDay.asInstanceOf[Integer]
+    val maxDate = LocalDate.of(2017, 11, 10).toEpochDay.asInstanceOf[Integer]
+    // mount the real random date !
+    val random = Integer.toString(new Random().nextInt(5) + (maxDate.intValue() - minDate.intValue()))
+    // create the request
+    random
+  }
+
+
+  def buildCarDate(session: Session): String = {
+    val date = session("date").as[String]
+    raw"""{
+      "company" : "Tesla",
+      "city" : "Nice",
+      "model" : "S",
+      "numberPlate" : "999-111",
+      "bookedDays" : [{ "date" : "$date" }]
+      }
+      """"
+  }
+
+  def buildCarModel(session: Session): String = {
     val model = session("model").as[String]
     raw"""{
       "company" : "Tesla",
@@ -43,12 +79,5 @@ class CarSimulation extends Simulation {
 
   }
 
-  def buildRetrieve(session: Session): String = {
-    val model = session("model").as[String]
-    raw"""{
-      "model": "$model"
-    }""""
-
-  }
   setUp(stressSample.inject(rampUsers(20) over (10 seconds)).protocols(httpConf))
 }
