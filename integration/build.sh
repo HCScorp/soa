dccce8d811a8c4e4c20842cc88f2570d8a7f388b
Loïck MAHIEUX <loick111@gmail.com>
2017-10-24 16:50:36 +0200

    Fix integration build.sh

--- integration/build.sh
+++ integration/build.sh
@@ -3,5 +3,5 @@
 mvn clean package -DskipTests
 rm ../deployment/data/deploy/flows-1.0.jar
 
-mkdir -r ../deployment/data/deploy
+mkdir -p ../deployment/data/deploy
 cp flows/target/flows-1.0.jar ../deployment/data/deploy/flows-1.0.jar
