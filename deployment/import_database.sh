53ba2e5af11c9f7cc8c62c5a56350a48d094dc47
Loïck MAHIEUX <loick111@gmail.com>
2017-11-08 00:13:13 +0100

    Import data into MongoDB

---
+++ deployment/import_database.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "# Import cars"
+docker-compose exec database /bin/bash -c "mongoimport --db car --collection cars --jsonArray --file /import/cars.json"
+
+echo "# Import flights"
+docker-compose exec database /bin/bash -c "mongoimport --db flight --collection flights --jsonArray --file /import/flights.json"
+
+echo "# Import hotels"
+docker-compose exec database /bin/bash -c "mongoimport --db hotel --collection hotels --jsonArray --file /import/hotels.json"
