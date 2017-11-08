06c74433a321623cef1c848a7062b80454dca654
cesar colle <cesar.colle@gmail.com>
2017-11-08 21:22:32 +0100

    add config ftp

--- integration/flows/src/main/java/fr/unice/polytech/hcs/flows/refund/RefundArchiver.java
+++ integration/flows/src/main/java/fr/unice/polytech/hcs/flows/refund/RefundArchiver.java
@@ -16,7 +16,6 @@ public class RefundArchiver extends RouteBuilder {
                 .routeDescription("where to send your refund piece")
                 .routeId("refund-piece-route")
                 .marshal().json(JsonLibrary.Jackson)
-                //.to("file://output?fileName=out.json");
-                .to("ftp://localhost:11021/out?username=scott&password=tiger&passiveMode=true&binary=true&fileName=out.json");
+                .to("ftp://localhost:11021/out?username=scott&password=tiger&binary=true&fileName=out.json");
     }
 }
