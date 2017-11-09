6379b3bedc67f3c10d222ea0713be99fd8a997c1
Thomas Munoz <thomas.munoz@etu.unice.fr>
2017-11-09 02:03:24 +0100

    Change Expense name and add travel status

--- integration/flows/src/main/java/fr/unice/polytech/hcs/flows/expense/Expense.java
+++ integration/flows/src/main/java/fr/unice/polytech/hcs/flows/expense/Expense.java
@@ -3,35 +3,38 @@ package fr.unice.polytech.hcs.flows.expense;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
-import java.util.List;
 
-public class Expense implements Serializable {
-    @JsonProperty public Integer travelId;
-    @JsonProperty public List<Document> documents;
+public class Expense implements Serializable{
+    @JsonProperty public String category;
+    @JsonProperty public String evidence;
+    @JsonProperty public Integer price;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
 
-        Expense expense = (Expense) o;
+        Expense document = (Expense) o;
 
-        if (travelId != expense.travelId) return false;
-        return documents != null ? documents.equals(expense.documents) : expense.documents == null;
+        if (price != document.price) return false;
+        if (category != null ? !category.equals(document.category) : document.category != null) return false;
+        return evidence != null ? evidence.equals(document.evidence) : document.evidence == null;
     }
 
     @Override
     public int hashCode() {
-        int result = travelId;
-        result = 31 * result + (documents != null ? documents.hashCode() : 0);
+        int result = category != null ? category.hashCode() : 0;
+        result = 31 * result + (evidence != null ? evidence.hashCode() : 0);
+        result = 31 * result + price;
         return result;
     }
 
     @Override
     public String toString() {
         return "Expense{" +
-                "travelId=" + travelId +
-                ", documents=" + documents +
+                "category='" + category + '\'' +
+                ", evidence='" + evidence + '\'' +
+                ", price=" + price +
                 '}';
     }
 }
