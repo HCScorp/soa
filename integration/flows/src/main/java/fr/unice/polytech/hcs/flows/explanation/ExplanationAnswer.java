247ab9ad7945e294aa4a0c976031ad39be24c692
cesarcolle <cesar.colle@gmail.com>
2017-11-11 14:49:28 +0000

    Add the route to choice between approuve or not a refunding

--- integration/flows/src/main/java/fr/unice/polytech/hcs/flows/explanation/ExplanationAnswer.java
+++ integration/flows/src/main/java/fr/unice/polytech/hcs/flows/explanation/ExplanationAnswer.java
@@ -2,7 +2,25 @@ package fr.unice.polytech.hcs.flows.explanation;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class ExplanationAnswer extends Explanation {
-
+public class ExplanationAnswer  {
+    @JsonProperty public String id;
     @JsonProperty public int code;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ExplanationAnswer that = (ExplanationAnswer) o;
+
+        if (code != that.code) return false;
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + code;
+        return result;
+    }
 }
--- integration/flows/src/main/java/fr/unice/polytech/hcs/flows/explanation/ExplanationProvider.java
+++ integration/flows/src/main/java/fr/unice/polytech/hcs/flows/explanation/ExplanationProvider.java
@@ -1,10 +1,15 @@
 package fr.unice.polytech.hcs.flows.explanation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.unice.polytech.hcs.flows.expense.Expense;
+import fr.unice.polytech.hcs.flows.expense.Travel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
 import org.bson.types.ObjectId;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import static fr.unice.polytech.hcs.flows.utils.Endpoints.*;
 
@@ -21,6 +26,8 @@ public class ExplanationProvider extends RouteBuilder {
                 .to(EXPLANATION_PROVIDER)
         ;
 
+
+        // Explanation for why did you use so much money dude.
         from(EXPLANATION_PROVIDER)
                 .routeId("explanation-provider")
                 .log("[" + EXPLANATION_PROVIDER + "] Received explanation")
@@ -29,40 +36,100 @@ public class ExplanationProvider extends RouteBuilder {
                 .removeHeaders("CamelHttp*")
 
                 .log("[" + EXPLANATION_PROVIDER + "] Prepare request parameters for DB search route")
+
+                //User send a Explanation POJO to excuse .
                 .unmarshal().json(JsonLibrary.Jackson, Explanation.class)
-                .process(e -> e.getIn().setBody(
-                        Collections.singletonMap("_id", new ObjectId(e.getIn().getBody(Explanation.class).id))))
+
+                .process(e -> {
+
+                    //We we'll use directly a explanation object.
+                    Explanation explanation = e.getIn().getBody(Explanation.class);
+
+                    // We need the travel ID from mongo DB to catch the travel that the user speak about.
+                    e.getIn().setBody(
+                            Collections.singletonMap("_id", new ObjectId(explanation.id)));
+
+                    // The explanation that we'll use later to accept or not.
+                    e.getIn().setHeader("explanation", explanation.explanation);
+
+                })
 
                 .log("[" + EXPLANATION_PROVIDER + "] Send to DB search route")
                 .inOut(GET_TRAVEL)
-
+                .removeHeader("CamelHttp*")
                 .log("[ " + EXPLANATION_PROVIDER + "]" + "UTILISER LE OOOOOOOUUUUUUUTTTTTTT qui est un MongoDBObject (une map++)")
+
+                .process(exchange -> {
+
+                    // Cast to a MAP because the POJO is quiet complexe.
+                    Map body = exchange.getIn().getBody(Map.class);
+                    body.put("explanation", exchange.getIn().getHeader("explanation") );
+                    
+                    exchange.getIn().setBody(body);
+                    System.out.println("send : " + body);
+                })
+        .log("[ " + EXPLANATION_PROVIDER + "]" + "Object updated  !")
+
+        .to(SAVE_TRAVEL_DATABASE_EP)
+
         ;
 
 //
-//        from(EXPLANATION_ANSWER)
-//                .choice()
-//                .when(simple("${header.code} == 1"))
-//                .process(exchange -> {
-//                    ExplanationAnswer explanationAnswer = exchange.getIn().getBody(ExplanationAnswer.class);
-//                    exchange.getIn().setHeader("code", explanationAnswer.code);
-//                     exchange.getIn().setBody(explanationAnswer.code);
-//                })
-//                .to(REFUND_ACCEPT)
-//                .when(simple("${header.code} == 0"))
-//                .to(EXPLANATION_REFUSED);
-//
-//
-//        from(EXPLANATION_REFUSED)
-//                .setHeader(Exchange.HTTP_METHOD, constant("POST"))
-//                .setHeader("Content-Type", constant("application/json"))
-//                .setHeader("Accept", constant("application/json"))
-//                .to(EXPLANATION_REFUSED_EP);
-//
-//
-//
-//        from(REFUND_ACCEPT)
-//                .log("refund accepted !");
+
+        restConfiguration()
+                .component("servlet")
+        ;
+
+        rest("/explanation")
+                .post("/answer")
+                .to(EXPLANATION_ANSWER)
+        ;
+        from(EXPLANATION_ANSWER)
+                .routeId("explanation-answer")
+                .routeDescription("answer to an explanation.")
+                .log("[" + EXPLANATION_ANSWER + "] Received answer")
+
+                .log("[" + EXPLANATION_ANSWER+ "] Remove shitty headers (thx camel)")
+                .removeHeaders("CamelHttp*")
+
+                .log("[" + EXPLANATION_ANSWER + "] Prepare request parameters for DB search travel")
+                .unmarshal().json(JsonLibrary.Jackson, ExplanationAnswer.class)
+                .process(e -> {
+                    ExplanationAnswer explanationAnswer = e.getIn().getBody(ExplanationAnswer.class);
+                    e.getIn().setBody(
+                            Collections.singletonMap("_id", new ObjectId(explanationAnswer.id)));
+                    e.getIn().setHeader("code", Integer.toString(explanationAnswer.code));
+                })
+
+                .log("[" + EXPLANATION_ANSWER + "]" + "request to the database send.")
+                .inOut(GET_TRAVEL)
+                .log("[" + EXPLANATION_ANSWER + "]" + "Value retrieve from Db !")
+
+                .process(exchange -> {
+                    Map body = exchange.getIn().getBody(Map.class);
+                    exchange.getIn().setHeader("explanation", body.get("explanation"));
+
+                })
+                .log("[" + EXPLANATION_ANSWER + "]" + "Make a choice.")
+
+                .choice()
+                .when(simple("${header.code} == 1"))
+                .process(exchange -> {
+                    Map body = exchange.getIn().getBody(Map.class);
+                    Travel travel = new Travel();
+                    travel.documents = (List<Expense>) body.get("documents");
+                    travel.status = (String) body.get("status");
+                    travel.travelId = (Integer) body.get("travelId");
+
+
+                    exchange.getIn().setBody(travel);
+                })
+                .log("ACCEPT : refundement : ${header.explanation} is correct and accepted, well done :D")
+                .to(ACCEPT_REFUND)
+                .when(simple("${header.code} == 0"))
+                .log("ERROR refundement : ${header.explanation} is not correct for your manager ;-) " );
+
+
 
 
     }
